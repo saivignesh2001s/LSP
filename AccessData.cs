1d593946faa253210e1511e62f0465f47635e038
saivignesh.p <saivignesh.p@LTPCHE032529213>
2023-08-23 20:49:49 +0530

    Add project files.

---
+++ AccessData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    internal class AccessData
+    {
+        public virtual void Read()
+        {
+            Console.WriteLine("Reading the file");
+        }
+        public virtual void Write() {
+            Console.WriteLine("Writing the file");
+        }
+    }
+    public interface IFileRead
+    {
+        void Read();
+    }
+    public interface IFileWrite
+    {
+        void Write();
+    }
+    internal class Adminaccess : IFileRead, IFileWrite
+    {
+        public void Read()
+        {
+           Console.WriteLine("Reading the file");
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Writing the file");
+        }
+    }
+    internal class Normalaccess : IFileRead
+    {
+        public void Read()
+        {
+            Console.WriteLine("Reading the file");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LSP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            IFileRead rs = new Normalaccess();
+            rs.Read();
+            IFileWrite rf = new Adminaccess();
+            IFileRead rd = new Adminaccess();
+            rd.Read();
+            rf.Write();
+
+        }
+    }
+}
